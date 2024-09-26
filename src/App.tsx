e978ab27bd4bfba96120fa4bceb58a18deaa1cc1
Lukas <lukonelis1@gmail.com>
2024-09-26 14:16:27 +0300

    adjusted spacing

--- src/App.tsx
+++ src/App.tsx
@@ -12,14 +12,14 @@ function App() {
       <div className='hidden lg:block'>
         <div className='lg:flex lg:justify-center'>
           <div className='lg:w-full'>
-            <div className='pr-9 max-w-[520px] ml-auto'>
+            <div className='pr-9 max-w-[550px] ml-auto'>
               <Contact />
               <Delivery />
               <Payment />
             </div>
           </div>
           <div className='bg-neutral-100 lg:w-full'>
-            <div className='border-l pl-9 max-w-[505px] mr-auto'>
+            <div className='border-l pl-9 max-w-[535px] mr-auto'>
               <div className='hidden lg:block'>
                 <OrderInfo />
               </div>
